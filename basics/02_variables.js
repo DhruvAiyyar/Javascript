8b3a93186b2f3d084de62e7b808f724dbc9c6c84
Dhruv <dhruvsmac@gmail.com>
2025-11-01 18:02:41 +0000

    variables and datatypes

---
+++ basics/02_variables.js
@@ -0,0 +1,21 @@
+"use strict"; //JS now runs in newer version
+
+//alert(3+3) // wont work as we are using node js and not browser
+
+let name = "Dhruv"
+let age = 14
+// there are different datatypes 
+//string
+// integer and float
+//bigint
+//null
+//boolean True and False
+//undefined (means value not assigned)
+// symbol (helps for uniqueness of a thing)
+
+
+// object 
+
+// typeof ==> this tells type of given variable 
+console.log(typeof "Dhruv")
+
